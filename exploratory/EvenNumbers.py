1086b141f6423a570d71b8a0f336e42c84dfa326
raginiyampalli <yraginireddy@gmail.com>
2024-02-21 16:01:01 -0500

    mandatory hello world

---
+++ exploratory/EvenNumbers.py
@@ -0,0 +1,7 @@
+def print_even_numbers(a, b):
+    for i in range(a, b, 1):
+        if i % 2 == 0:
+            print(i)
+
+
+print_even_numbers(5,50)
---
+++ exploratory/EvenOrNot.py
@@ -0,0 +1,10 @@
+def is_even(a):
+    if a % 2 == 0:
+        return True
+    return False
+
+
+print(is_even(35))
+
+print(is_even(36))
+
