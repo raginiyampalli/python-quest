ca7b5601a7dbb1dd9b00097d8f0707797352d485
raginiyampalli <yraginireddy@gmail.com>
2024-02-24 23:16:32 -0500

    External helper functions playground.

--- exploratory/FibonacciSeriesRecursion.py
+++ exploratory/FibonacciSeriesRecursion.py
@@ -1,3 +1,4 @@
+import HelperFunctions
 def fibonacci_series(target, num1, num2, series):
     if target == 0:
         series.append(num1 + num2)
@@ -9,5 +10,5 @@ def fibonacci_series(target, num1, num2, series):
 """Testing above function"""
 series = list()
 fibonacci_series(5, 0, 1, series)
-for num in series:
-    print(num)
+
+HelperFunctions.print_numbers(series)
---
+++ exploratory/HelperFunctions.py
@@ -0,0 +1,4 @@
+def print_numbers(arr):
+    print('Printing using helper function')
+    for v in arr:
+        print(v)
